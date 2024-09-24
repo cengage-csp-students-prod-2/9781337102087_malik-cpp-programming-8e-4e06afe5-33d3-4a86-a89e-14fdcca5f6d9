91384e03e21d2e526f961323a32baa407d5db5a7
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-09-24 23:13:45 +0000

    Testing Tue Sep 24 2024 23:13:45 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/main.cpp
+++ chapter3/ex01/student/main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     // Write your main here
+    ifstream inFile;
+    ofstream outFile;
+
+    inFile.open("inData.txt");
+    outFile.open("outData.txt");
+
+    string firstname;
+    string lastname;
+    string department;
+
+    double salary;
+    double bonus;
+    double taxes;
+    double paycheck;
+
+    double distance;
+    double time;
+    double avg;
+
+    double coffee;
+    double cost;
+    double sales;
+
+    inFile >> firstname >> lastname >> department;
+    
+    outFile << "Name: " << firstname << " " << 
+      lastname << ", " << "Department: " << 
+        department << endl;
+
+
+
+            
+
+
     return 0;
 }
