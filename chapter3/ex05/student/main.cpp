66b0bbd4e459fbf92d8847cb9f97977e96a17dc9
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-03 15:36:43 +0000

    Testing Thu Oct 03 2024 15:36:43 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/main.cpp
+++ chapter3/ex05/student/main.cpp
@@ -16,24 +16,28 @@ int main() {
 
     double salary1, salary2, salary3;
     double payincrease1, payincrease2, payincrease3;
+    double updatedsalary1, updatedsalary2, updatedsalary3;
 
     inFile >> lastnameM >> firstnameA_M >> salary1 >> payincrease1;
     inFile >> lastnameG >> firstnameS >> salary2 >> payincrease2;
     inFile >> lastnameS >> firstnameA_S >> salary3 >> payincrease3;
 
     
-    salary1 += payincrease1;
-    salary2 += payincrease2;
-    salary3 += payincrease3;
+    updatedsalary1 = salary1 + (salary1 * (payincrease1/100));
+    updatedsalary2 = salary2 + (salary2 * (payincrease2/100));
+    updatedsalary3 = salary3 + (salary3 * (payincrease3/100));
 
     
     outFile << fixed << setprecision(2);
-    outFile << "Employee: " << firstnameA_M << " " << lastnameM << "\n"
-            << "New Salary: " << salary1 << "\n\n";
-    outFile << "Employee: " << firstnameS << " " << lastnameG << "\n"
-            << "New Salary: " << salary2 << "\n\n";
-    outFile << "Employee: " << firstnameA_S << " " << lastnameS << "\n"
-            << "New Salary: " << salary3 << "\n";
+    outFile << "Employee: " << firstnameA_M << " " << lastnameM << 
+       "\nPay increase: %" << payincrease1 <<"\n"
+            << "New Salary: $" << salary1 << "\n\n";
+    outFile << "Employee: " << firstnameS << " " << lastnameG <<
+       "\nPay increase: %" << payincrease2 << "\n"
+            << "New Salary: $" << salary2 << "\n\n";
+    outFile << "Employee: " << firstnameA_S << " " << lastnameS <<
+       "\nPay increase: %" << payincrease3 << "\n"
+            << "New Salary: $" << salary3 << "\n";
 
     
     inFile.close();
