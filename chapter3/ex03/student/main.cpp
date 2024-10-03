eb6663cf0020443b81373246a156759ecb68154e
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-03 02:39:13 +0000

    Testing Thu Oct 03 2024 02:39:13 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex03/student/main.cpp
+++ chapter3/ex03/student/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     // Write your main here
+
+    double kilos;
+
+    cout << "enter weight in kilos: ";
+    cin >> kilos;
+
+
+    cout << "pounds equivalent: " << kilos * 2.2 << endl;
+
+
+
+
+
     return 0;
 }
