0c35b15881322623f56913d78f9a5337419de86d
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-03 18:01:39 +0000

    Testing Thu Oct 03 2024 18:01:39 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/main.cpp
+++ chapter4/ex01/student/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     // Write your main here
+
+    int num;
+
+    cout << "input number: ";
+    cin >> num;
+
+    if(num > 0)   
+      cout << "positive\n";
+    else if(num < 0)
+      cout << "negative\n";
+    if (num == 0)
+       cout << "zero\n";
+
+
+    
+
+
+
     return 0;
 }
