0cca1d45ae856b28443de1941567662fda128df2
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-03 02:56:33 +0000

    Testing Thu Oct 03 2024 02:56:33 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/main.cpp
+++ chapter3/ex04/student/main.cpp
@@ -1,4 +1,3 @@
-//Logic errors.
 #include <iostream>
 #include <iomanip>
 
@@ -27,10 +26,13 @@ int main()
     cin >> area;
     cout << endl;
     
-    cout << "The cost of the fertilizer per pound is: $"
-         << bagSize / cost << endl;
+    // Correcting the logic for cost per pound
+    double costPerPound = cost / bagSize;
+    cout << "The cost of the fertilizer per pound is: $" << costPerPound << endl;
     
-    cout << "The cost of fertilizing per square foot is: $"
-         << area / cost << endl;
+    // Correcting the logic for cost per square foot
+    double costPerSquareFoot = cost / area;
+    cout << "The cost of fertilizing per square foot is: $" << costPerSquareFoot << endl;
+
     return 0;
 }
