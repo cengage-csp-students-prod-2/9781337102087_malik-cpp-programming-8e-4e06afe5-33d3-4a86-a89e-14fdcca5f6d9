6c31dcd34379bfe5088b849b882c8c4e2aaf8db6
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-08 17:51:18 +0000

    Testing Tue Oct 08 2024 17:51:18 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/main.cpp
+++ chapter4/ex02/student/main.cpp
@@ -14,6 +14,22 @@ int main() {
     cout << "Enter three numbers: ";
     cin >> num1 >> num2 >> num3;
 
+    if (num1 <= num2 && num1 <=num3){
+         cout << " " << num1 << " "  << num2 << " " << num3;
+    }
+
+    if (num2 <= num1 && num2 <= num3){
+        cout << " " << num2 << " " << num3 << " " << num1;
+    }
+ 
+    if(num3 <= num1 && num3 <= num2){
+        cout << " " << num3 << " " << num2 << " " << num1;
+    }
+  
+    
+    
+
+
 
 
 
