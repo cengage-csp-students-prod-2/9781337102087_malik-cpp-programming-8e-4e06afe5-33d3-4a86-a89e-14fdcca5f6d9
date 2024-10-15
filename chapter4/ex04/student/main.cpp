cfcffcb8d7034939ac31229c159a6eaa21c992c6
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-15 17:14:34 +0000

    Testing Tue Oct 15 2024 17:14:34 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/main.cpp
+++ chapter4/ex04/student/main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 int main()
 {
     string shape;
-    double height, width, length, radius;
+    double height;
+    double width;
+    double length;
+    double radius;
     const double PI = 3.1416;
 
     cout << "Enter the shape type: (rectangle, circle, cylinder) ";
@@ -17,45 +20,54 @@ int main()
 
     if (shape == "rectangle")
     {
-        
         cout << "Enter the length of the rectangle: ";
         cin >> length;
+        cout << endl;
+
         cout << "Enter the width of the rectangle: ";
         cin >> width;
         cout << endl;
 
-        
-        cout << "Area of the rectangle = " << length * width << endl;
-        cout << "Perimeter of the rectangle = " << 2 * (length + width) << endl;
+        cout << fixed << showpoint << setprecision(2);
+        cout << "Area of the rectangle = "
+             << length * width << endl;
+
+        cout << "Perimeter of the rectangle = "
+             << 2 * (length + width) << endl;
     }
     else if (shape == "circle")
     {
-        
         cout << "Enter the radius of the circle: ";
         cin >> radius;
         cout << endl;
 
-        
-        cout << "Area of the circle = " << PI * pow(radius, 2.0) << endl;
-        cout << "Circumference of the circle = " << 2 * PI * radius << endl;
+        cout << fixed << showpoint << setprecision(2);
+        cout << "Area of the circle = "
+             << PI * pow(radius, 2.0) << endl;
+
+        cout << "Circumference of the circle = "
+             << 2 * PI * radius << endl;
     }
     else if (shape == "cylinder")
     {
-        
         cout << "Enter the height of the cylinder: ";
         cin >> height;
+        cout << endl;
+
         cout << "Enter the radius of the base of the cylinder: ";
         cin >> radius;
         cout << endl;
 
-        
-        cout << "Volume of the cylinder = " << PI * pow(radius, 2.0) * height << endl;
-        cout << "Surface area of the cylinder = " << 2 * PI * radius * height + 2 * PI * pow(radius, 2.0) << endl;
+        cout << fixed << showpoint << setprecision(2);
+        cout << "Volume of the cylinder = "
+             << PI * pow(radius, 2.0) * height << endl;
+
+        cout << "Surface area of the cylinder = "
+             << 2 * PI * radius * height + 2 * PI * pow(radius, 2.0) << endl;
     }
     else
     {
-       
-        cout << "The program does not handle the shape: " << shape << endl;
+        cout << "The program does not handle " << shape << endl;
     }
 
     return 0;
