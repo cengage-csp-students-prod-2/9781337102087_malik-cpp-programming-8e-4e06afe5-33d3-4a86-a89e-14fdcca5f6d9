d47786bbde0687b6415726374ee1342b15a7812f
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-15 16:57:11 +0000

    Testing Tue Oct 15 2024 16:57:11 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/main.cpp
+++ chapter4/ex03/student/main.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-    // Write your main here
-    return 0;
+    int num;
+
+    cout << "Enter an integer between 0 and 35: ";
+    cin >> num;
+    
+    if (num < 0 || num > 35) {
+        cout << "Number must be between 0 and 35." << endl;
+        return 1;}
+    
+    if (num <= 9) {
+        cout << num << endl;} 
+        
+        else {
+        
+        char letter = static_cast<char>('A' + (num - 10));
+        cout << letter << " for " << num << endl;}
+
+return 0;
 }
