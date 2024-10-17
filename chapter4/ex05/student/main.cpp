a8c1cde3009ebf5bd0b9990b7318a9e44f673d04
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-17 03:36:17 +0000

    Testing Thu Oct 17 2024 03:36:17 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex05/student/main.cpp
+++ chapter4/ex05/student/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     // Write your main here
+
+    int side1, side2, side3;
+    string rtriangle;
+
+    cout << "enter length of three sides of a triangle: " << endl;
+    cin >> side1 >> side2>> side3;
+
+   
+
+    if (side1 == side2+side3){ cout << "it is a right angled triangle."<<endl;
+
+    }
+    else {
+        cout << "it is not a right angled triangle.";
+    }
+
+    
+
+
+
     return 0;
 }
