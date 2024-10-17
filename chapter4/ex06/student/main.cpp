07fffbf9b70fb9e9aa9679cf1dc71bff0778346e
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-17 04:06:52 +0000

    Testing Thu Oct 17 2024 04:06:52 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/main.cpp
+++ chapter4/ex06/student/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     // Write your main here
+    int cookies;
+    const int cookiesPerBox = 24;
+    const int boxesPerContainer = 75;
+    
+   
+
+
+
+    cout << "Total Number of Cookies: ";
+    cin >> cookies;
+
+    int totalboxes = cookies/cookiesPerBox;
+    int totalcontainers = totalboxes/boxesPerContainer;
+
+    cout << "Total Boxes Needed: " << totalboxes <<"\nTotal Containers Needed: " << totalcontainers << endl;
+    
+
+
+
+
+
     return 0;
 }
