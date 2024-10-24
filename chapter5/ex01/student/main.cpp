b27afc01788db36fe08a6b02d3a579963bf430a7
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-24 17:08:38 +0000

    Testing Thu Oct 24 2024 17:08:38 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex01/student/main.cpp
+++ chapter5/ex01/student/main.cpp
@@ -3,6 +3,35 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int num;
+    int sum = 0;
+    int digit;
+
+    cout << "Please enter an integer: ";
+    cin >> num;
+
+    
+    if (num < 0) num = -num;
+
+    cout << "The individual digits are: ";
+
+    
+    if (num == 0) {
+        cout << 0 << endl;
+        sum = 0;
+    } else {
+        
+        while (num > 0) {
+            digit = num % 10; 
+            cout << digit << " ";  
+            sum += digit;        
+            num /= 10;             
+        }
+        cout << endl;
+    }
+
+    
+    cout << "The sum of the digits is: " << sum << endl;
+
     return 0;
 }
